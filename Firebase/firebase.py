64d4be9edeb436342843a97ec909ca1fd3b7dc7c
Ferno22 <tonyfer002@gmail.com>
2023-04-18 20:49:18 +0200

    fix creds

--- Firebase/firebase.py
+++ Firebase/firebase.py
@@ -1,4 +1,5 @@
 import os
+import base64
 import firebase_admin
 from firebase_admin import credentials
 from dotenv import load_dotenv
@@ -6,4 +7,11 @@ from dotenv import load_dotenv
 # Load the service account key from an environment variable
 load_dotenv()
 cert = os.environ.get('SERVICE_ACCOUNT_KEY')
-firebase_admin.initialize_app(cert)
+
+# Decode and write the service account key to a json file
+with open('serviceAccountKey.json', 'wb') as f:
+  f.write(base64.b64decode(cert))
+
+# Initialize from the json file
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred)
