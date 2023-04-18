c8a4920b8edd0147a4137cd5cb1d1dd5a8c7558a
Ferno22 <tonyfer002@gmail.com>
2023-04-18 21:09:19 +0200

    another fix

--- Firebase/firebase.py
+++ Firebase/firebase.py
@@ -1,17 +1,20 @@
-import os
-import base64
 import firebase_admin
 from firebase_admin import credentials
-from dotenv import load_dotenv
+import json
+import subprocess
 
-# Load the service account key from an environment variable
-load_dotenv()
-cert = os.environ.get('SERVICE_ACCOUNT_KEY')
+# Specify the encoding when opening the file
+with open('serviceAccountKey.json', encoding='latin1') as f: # or whatever encoding your file uses
+  cert = json.load(f)
+cred = credentials.Certificate(cert)
+firebase_admin.initialize_app(cred)
 
-# Decode and write the service account key to a json file
-with open('serviceAccountKey.json', 'wb') as f:
-  f.write(base64.b64decode(cert))
+# Run iconv command using subprocess
+subprocess.run(['iconv', '-f', 'latin1', '-t', 'utf8', 'serviceAccountKey.json', '>', 'serviceAccountKey_utf8.json'])
 
-# Initialize from the json file
-cred = credentials.Certificate('serviceAccountKey.json')
+# Run recode command using subprocess
+subprocess.run(['recode', 'latin1..utf8', 'inputfile'])
+
+# Then use the converted file as usual
+cred = credentials.Certificate('serviceAccountKey_utf8.json')
 firebase_admin.initialize_app(cred)
