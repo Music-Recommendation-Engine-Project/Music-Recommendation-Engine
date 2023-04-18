f1564a7855f112b8ec25a9537ccf434e19539f13
Ferno22 <tonyfer002@gmail.com>
2023-04-18 20:10:54 +0200

    changed secret name

--- Firebase/firebase.py
+++ Firebase/firebase.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("${{ secrets.SERVICE_ACCOUNT_KEY }}")
+cred = credentials.Certificate("${{ secrets.SAK }}")
 firebase_admin.initialize_app(cred)
