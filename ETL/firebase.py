e9dfa43027be4d5fe3454d2306ae4813f708abc3
Juan Rubiales <61597804+jrubialesv@users.noreply.github.com>
2023-04-19 20:30:59 +0200

    Credentials modified to secrets

--- ETL/firebase.py
+++ ETL/firebase.py
@@ -1,24 +1,6 @@
 import pyrebase
 
-firebaseConfig = {
-  'apiKey': "AIzaSyCRK2mklm8HyvUTI-zfVcaHaRTTXy3t1mE",
-
-  'authDomain': "music-reco-c4de1.firebaseapp.com",
-
-  'databaseURL': "https://music-reco-c4de1-default-rtdb.firebaseio.com",
-
-  'projectId': "music-reco-c4de1",
-
-  'storageBucket': "music-reco-c4de1.appspot.com",
-
-  'messagingSenderId': "1008354595590",
-
-  'appId': "1:1008354595590:web:bfd443a817501051604487",
-
-  'measurementId': "G-VJS6DZJGBK"
-}
-
-firebase = pyrebase.initialize_app(firebaseConfig)
+firebase = pyrebase.initialize_app(os.getenv("FIREBASE_APP_KEY"))
 
 storage = firebase.storage()
 
