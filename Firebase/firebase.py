8eefd5eccb96364a0e319cc686f02fc7d687c3d5
Ferno22 <tonyfer002@gmail.com>
2023-04-19 16:07:00 +0200

    rematch

--- Firebase/firebase.py
+++ Firebase/firebase.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("../Firebase/serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
